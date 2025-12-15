c96ee9e2a7b1667ecbb6276b7da6293e1edbc12a
Julian Abhari <julian.abhari20@gmail.com>
2025-12-14 19:21:02 -0600

    first commit

---
+++ sketch.js
@@ -0,0 +1,55 @@
+// Array to store the points of the heart shape
+let heart = [];
+
+// Variable to control the angle for the heart shape calculation
+let a = 0;
+
+// The setup function is called once when the program starts
+function setup() {
+  // Create a canvas of 800x800 pixels
+  createCanvas(800, 800);
+  // Set the pixel density to 2 for high-resolution displays
+  pixelDensity(2);
+}
+
+// The draw function is called continuously in a loop
+function draw() {
+  // Set the background color to black
+  background(0);
+  // Move the origin to the center of the canvas
+  translate(width / 2, height / 2);
+
+  // Disable filling shapes
+  noFill();
+  // Set the stroke color to white
+  stroke(255);
+  // Set the stroke weight to 4 pixels
+  strokeWeight(4);
+  // Set the fill color to a shade of purple
+  fill(150, 0, 100);
+
+  // Begin drawing a shape
+  beginShape();
+  // Loop through each point in the heart array and create a vertex
+  for (let vector of heart) {
+    vertex(vector.x, vector.y);
+  }
+  // End drawing the shape
+  endShape();
+
+  // Radius for the heart shape calculation
+  let r = 10;
+  // Calculate the x-coordinate of the next point in the heart shape
+  let x = r * 16 * pow(sin(a), 3);
+  // Calculate the y-coordinate of the next point in the heart shape
+  let y = -r * (13 * cos(a) - 5 * cos(2 * a) - 2 * cos(3 * a) - cos(4 * a));
+  // Add the new point to the heart array
+  heart.push(createVector(x, y));
+  a += 0.02;
+
+  for (var i = 0; i < vehicles.length; i += 1) {
+    vehicles[i].applyForce(vehicles[i].arrive());
+    vehicles[i].update();
+    vehicles[i].show();
+  }
+}
